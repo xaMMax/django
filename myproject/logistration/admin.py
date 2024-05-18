615944d2bc7c52344d2525a9f187ffb7f7c9dcae
xaMMax <petrolijuice@gmail.com>
2024-05-18 21:17:35 +0200

    add additional code comments.

--- myproject/logistration/admin.py
+++ myproject/logistration/admin.py
@@ -4,19 +4,16 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import UserProfile
 
-User = get_user_model()
-
-# Register your models here.
-# admin.site.register(UserProfile)
+User = get_user_model()  # Get the Django User model dynamically
 
 
 class UserProfileInline(admin.StackedInline):
-    model = UserProfile
+    model = UserProfile  # Inline model for UserProfile
 
 
 class CustomUserAdmin(UserAdmin):
-    inlines = (UserProfileInline,)
+    inlines = (UserProfileInline,)  # Add UserProfileInline to the UserAdmin
 
 
-admin.site.unregister(User)
-admin.site.register(User, CustomUserAdmin)
+admin.site.unregister(User)  # Unregister the default UserAdmin
+admin.site.register(User, CustomUserAdmin)  # Register the CustomUserAdmin with the User model
--- myproject/logistration/models.py
+++ myproject/logistration/models.py
@@ -4,28 +4,27 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-# Create your models here.
+# Create UserProfile model with extensions
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=12, unique=True)
-    bio = models.TextField(blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-    register_date = models.DateField(null=True, blank=True)
-    last_login_date = models.DateField(null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Reference to the Django User model
+    phone_number = models.CharField(max_length=12, unique=True)  # User's phone number
+    bio = models.TextField(blank=True)  # Short profile description (optional)
+    birth_date = models.DateField(null=True, blank=True)  # User's birthdate (optional)
+    register_date = models.DateField(null=True, blank=True)  # User's registration date (optional)
+    last_login_date = models.DateField(null=True, blank=True)  # User's last login date (optional)
 
     def __str__(self):
-        return str(self.user)
+        return str(self.user)  # Return the string representation of the user
 
 
+# Create a user profile when a new user is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
 
+# Save the user profile when user data is saved
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    # instance.profile.save()
-    instance.userprofile.save()
-
-
+    instance.userprofile.save()  # Save the user profile after saving user data
